22cde047b6cdb08e01e6182a6dce53b4b0f6de98
sarubo <sarubo2016@gmail.com>
2025-01-02 22:22:32 +0900

    Vite.AspNetCoreのExampleのvite.configに倣う

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import Pages from "vite-plugin-pages";
 import solidPlugin from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// Pattern for CSS files
+const cssPattern = /\.css$/;
+// Pattern for image files
+const imagePattern = /\.(png|jpe?g|gif|svg|webp|avif)$/;
+
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
@@ -11,6 +16,8 @@ export default defineConfig({
     }),
     solidPlugin(),
   ],
+  appType: "custom",
+  publicDir: "public",
   server: {
     port: 3000,
     proxy: {
@@ -22,6 +29,36 @@ export default defineConfig({
     },
   },
   build: {
+    manifest: true,
+    emptyOutDir: true,
+    outDir: "../wwwroot",
+    assetsDir: "",
+    rollupOptions: {
+      input: "src/index.tsx",
+      output: {
+        // Save entry files to the appropriate folder
+        entryFileNames: "js/[name].[hash].js",
+        // Save chunk files to the js folder
+        chunkFileNames: "js/[name]-chunk.js",
+        // Save asset files to the appropriate folder
+        assetFileNames: (info) => {
+          for (const name of info.names) {
+            // If the file is a CSS file, save it to the css folder
+            if (cssPattern.test(name)) {
+              return "css/[name][extname]";
+            }
+            // If the file is an image file, save it to the images folder
+            if (imagePattern.test(name)) {
+              return "images/[name][extname]";
+            }
+            // If the file is any other type of file, save it to the assets folder
+            return "assets/[name][extname]";
+          }
+          // If the file name is not specified, save it to the output directory
+          return "[name][extname]";
+        },
+      },
+    },
     target: "esnext",
   },
 });
